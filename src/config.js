1ad48ce7bbc1104d9993130c2c41b7a399ef23b4
reedanders <rean8874@colorado.edu>
2020-04-14 11:08:39 -0400

    finished react setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "gg-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://qt2u5n3zyd.execute-api.us-east-2.amazonaws.com/dev/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_wtb6TzgVG",
+    APP_CLIENT_ID: "7tket775lopks5tkqoee3fr0tb",
+    IDENTITY_POOL_ID: "s-east-2:21ddb891-42d4-42ef-b048-4aded2d9cf13"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>GoodGlobe</h1>
+        <p>Science-driven crowdfunding</p>
+      </div>
+    </div>
+  );
+}
