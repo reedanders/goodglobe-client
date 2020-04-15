402662748fd6abaf1c905e14c5dd1324f1610d74
reedanders <rean8874@colorado.edu>
2020-04-15 13:00:09 -0400

    api working, home working

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
+import { API } from "aws-amplify";
+import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
 
+
 export default function Home() {
+  const [projects, setProjects] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+	  async function onLoad() {
+	    if (!isAuthenticated) {
+	      return;
+	    }
+
+	    try {
+	      const projects = await loadProjects();
+	      setProjects(projects);
+	    } catch (e) {
+	      onError(e);
+	    }
+
+	    setIsLoading(false);
+	  }
+
+	  onLoad();
+	}, [isAuthenticated]);
+
+	function loadProjects() {
+	  return API.get("goodglobe", "/projects");
+	}
+
+  function renderProjectsList(projects) {
+	  return [{}].concat(projects).map((project, i) =>
+	    i !== 0 ? (
+	      <LinkContainer key={project.projectId} to={`/projects/${project.projectId}`}>
+	        <ListGroupItem header={project.content.trim().split("\n")[0]}>
+	          {"Created: " + new Date(project.createdAt).toLocaleString()}
+	        </ListGroupItem>
+	      </LinkContainer>
+	    ) : (
+	      <LinkContainer key="new" to="/projects/new">
+	        <ListGroupItem>
+	          <h4>
+	            <b>{"\uFF0B"}</b> Create a new project
+	          </h4>
+	        </ListGroupItem>
+	      </LinkContainer>
+	    )
+	  );
+	}
+
+  function renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple project taking app</p>
+      </div>
+    );
+  }
+
+  function renderProjects() {
+    return (
+      <div className="projects">
+        <PageHeader>Your Projects</PageHeader>
+        <ListGroup>
+          {!isLoading && renderProjectsList(projects)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   return (
     <div className="Home">
-      <div className="lander">
-        <h1>GoodGlobe</h1>
-        <p>Science-driven crowdfunding</p>
-      </div>
+      {isAuthenticated ? renderProjects() : renderLander()}
     </div>
   );
 }
