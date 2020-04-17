2612dfaf1b146aafdb42299d6da61b579bf27f24
reedanders <rean8874@colorado.edu>
2020-04-17 11:42:16 -0400

    adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,42 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({dsn: "https://6ce199c96caf43ab9ab83289bdab2184@o379411.ingest.sentry.io/5204236"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+
 export function onError(error) {
+  let errorInfo = {};
   let message = error.toString();
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
     message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
   }
+
+  logError(error, errorInfo);
 
   alert(message);
 }
