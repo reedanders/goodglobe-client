a02628c3ae7e5fbd9471ac47180382ea6112ae09
reedanders <rean8874@colorado.edu>
2020-04-17 11:19:35 -0400

    updating the navbar

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">GoodGlobe</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
