ad648fe18416630d112d51d59a673ea82d9ba241
reedanders <rean8874@colorado.edu>
2020-09-16 12:08:31 -0400

    adding carousel to card album with 4 cards

--- src/containers/MainFeaturedAlbum.js
+++ src/containers/MainFeaturedAlbum.js
@@ -33,8 +33,8 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3
+    items: 4,
+    slidesToSlide: 4
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
