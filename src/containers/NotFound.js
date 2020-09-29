11e4198825e6b96b550947e2dea3ce86c48f8d9f
reedanders <rean8874@colorado.edu>
2020-09-29 12:10:48 -0400

    updating 404

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,10 +1,41 @@
 import React from "react";
-import "./NotFound.css";
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import human_knot from '../assets/images/drawings/human_knot_SMALL.png';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(12),
+    paddingBottom: theme.spacing(4),
+  },
+  image: {
+  	padding: theme.spacing(8)
+  }
+}));
+
 
 export default function NotFound() {
-  return (
-    <div className="NotFound">
-      <h3>Sorry, page not found!</h3>
-    </div>
-  );
+
+	const classes = useStyles();
+
+	return (
+	<div className={classes.root}>
+	  <Grid container
+	  		direction="column"
+	  		justify="center"
+	  		alignItems="center">
+	  	<Grid item className={classes.image}>
+	  		<LazyLoadImage src={human_knot} alt="Tangled human 404" height="254"/>
+	  	</Grid>
+	  	<Grid item>
+	  		<Typography variant="h6">Hmmmmm! Looks like the page you are looking for does not exist.</Typography>
+	  	</Grid>
+	  </Grid>
+	</div>
+	);
 }
