0e78f3ee01a29dcc21bd62cec34eabe115d7095a
reedanders <rean8874@colorado.edu>
2020-10-21 16:57:04 -0400

    adding styles to postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,32 +1,58 @@
 import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
 import { Link }  from "react-router-dom"
 import Markdown from "react-markdown"
+
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
 import postlist from "../posts.json"
 
-const PostList = () => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        paddingTop: theme.spacing(4)
+    },
+    card: {
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2)
+    },
+    authorImage: {
+      width: theme.spacing(4),
+      height: theme.spacing(4),
+    }
+}));
+
+export default function PostList() {
+    const classes = useStyles();
+
     const excerptList = postlist.map(post => {
         return post.content.split(" ").slice(0, 20).join(" ") + "..."
     })
+
     return (
-        <div className="postlist">
-            {postlist.length && 
+        <div className={classes.root}>
+            {postlist.length ? 
                 postlist.map((post, i) => {
                     return (
-                        <div key={i} className="post-card">
-                             <div className="img-container">
-                                {post.thumbnail && <img className="thumbnail" width={80} src={post.thumbnail} alt=""/> }
-                                <h2 className="post-title"><Link className="links" to={`/post/${post.id}`}>{post.title}</Link></h2>
-                            </div>
-                            <small>Published on {post.date} by {post.author}</small>
+                        <Container key={i} className={classes.card}>
+                            <Grid items>
+                                <Typography variant="h6" component="h6">
+                                    <Link className="links" to={`/post/${post.id}`}>{post.title}</Link>
+                                </Typography>
+                            </Grid>
+                            <Grid items>
+                                <Typography variant="caption">Published on {post.date} by {post.author}</Typography>
+                            </Grid>
                             <hr/>
                             <Markdown source={excerptList[i]} escapeHtml={false} />
                             <small><Link className="links" to={`/post/${post.id}`}>Read more</Link></small>
-                        </div>
+                        </Container>
                     )
-                })
+                }) : <Typography component="h6" variant="h6" align="center" color="textPrimary" gutterBottom>
+                      Check back soon for blog posts on Goodglobe.
+                    </Typography>
             }
         </div>
-    )
+    );
 }
-
-export default PostList
