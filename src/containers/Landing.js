972a0ef6fbc6c1d22abc34d8869bbee5e80371f7
reedanders <rean8874@colorado.edu>
2020-10-30 13:47:26 -0400

    use a11y alt on hero image

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -37,12 +37,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const mainFeaturedProject = {
-  title: 'Title of a longer featured blog post',
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
   image: `url(${featured_image})`,
-  imgText: 'main image description',
-  linkText: 'Continue reading…',
+  imageText: 'collage of endangered plants and animals',
 };
 
 export default function Landing() {
